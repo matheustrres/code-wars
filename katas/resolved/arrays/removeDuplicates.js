7191c0eb7d749cd3b0b53c8d2e05d3eb0d92cd43
Matheus Torres <77691419+Torr7s@users.noreply.github.com>
2023-01-13 16:04:34 -0300

    Add method to remove duplicates from array of objects

--- katas/resolved/arrays/removeDuplicates.js
+++ katas/resolved/arrays/removeDuplicates.js
@@ -1,3 +1,28 @@
 const removeDuplicates = (arr) => [...new Set(arr)];
+const removeArrObjDuplicates = (arrObj) => [...new Set(arrObj.map(JSON.stringify))].map(JSON.parse);
+
+const arrObj = [
+    {
+        id: 2,
+        name: 'Torres'
+    },
+    {
+        id: 2,
+        name: 'Torres'
+    },
+    {
+        id: 1,
+        name: 'Andre'
+    },
+    {
+        id: 3,
+        name: 'Fernando'
+    },
+    {
+        id: 3,
+        name: 'Fernando'
+    }
+]
 
 console.log(removeDuplicates([0, 0, 0.55, 0,  1, 1, 2, 1, 3, 2])); /* [ 0, 0.55, 1, 2, 3 ] */
+console.log(removeArrObjDuplicates(arrObj)); 
